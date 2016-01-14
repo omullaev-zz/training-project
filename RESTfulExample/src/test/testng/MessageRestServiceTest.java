f84ae611da484b90553d6aa2ea9b02474ac4482a
Oleg Mullaev <omullaev@griddynamics.com>
2016-01-14 18:05:52 +0300

    Added test via RESTEasy native library with hamcrest matchers and the same test via REST Assured

---
+++ RESTfulExample/src/test/testng/MessageRestServiceTest.java
@@ -0,0 +1,29 @@
+package testng;
+
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.util.GenericType;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MessageRestServiceTest {
+
+    static final String ROOT_URL = "http://localhost:8080/RESTfulExample/rest/";
+
+    @Test
+    public void printMessageTest() throws Exception {
+
+//        Test via RESTEasy native library and hamcrest matchers
+        ClientRequest request = new ClientRequest(ROOT_URL +"message/hello");
+        ClientResponse<String> response = request.get(new GenericType<String>(){});
+        String result = response.getEntity();
+        assertThat("Method printMessage does not work correct", result, equalTo("Requested message: hello"));
+
+//        Test via REST Assured framework
+        get(ROOT_URL +"message/hello").then().assertThat().body(equalTo("Requested message: hello"));
+    }
+
+}
